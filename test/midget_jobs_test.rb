7b6ce86de0582d6ffd6d51ab5cd0929cb0d23087
josefchmel <chmel@jchsoft.cz>
2019-10-24 14:35:03 +0200

    test MidgetJobs.initialize_workers

--- test/midget_jobs_test.rb
+++ test/midget_jobs_test.rb
@@ -5,7 +5,18 @@ class MidgetJobsTest < Minitest::Test
     refute_nil ::MidgetJobs::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_initialize_workers
+    schedule_called = false
+    schedule_pr = Proc.new { schedule_called = true }
+    listen_notifications_called = false
+    listen_notifications_pr = Proc.new { listen_notifications_called = true }
+
+    MidgetJobs::Job.stub :schedule, schedule_pr do
+      MidgetJob.stub :listen_notifications, listen_notifications_pr do
+        MidgetJobs.initialize_workers
+        assert schedule_called, "proc not called"
+        assert listen_notifications_called, "proc not called"
+      end
+    end
   end
 end
