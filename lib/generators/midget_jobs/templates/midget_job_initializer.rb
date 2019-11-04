369e409f568fef6d36e627fe4612248150cf5f78
josefchmel <chmel@jchsoft.cz>
2019-11-04 17:10:21 +0100

    better server session detection for initializer

--- lib/generators/midget_jobs/templates/midget_job_initializer.rb
+++ lib/generators/midget_jobs/templates/midget_job_initializer.rb
@@ -1,7 +1,7 @@
 Rails.application.config.active_job.queue_adapter = :midget_job
 Rails.application.config.x.midget_jobs.at_once = 4
 
-if $0 =~ /rails/ || $0 =~ /puma/
+if Rails.const_defined? 'Server'
   Rails.application.config.after_initialize do
     MidgetJobs.initialize_workers
   end
