78a691b68fd9d0df6332046167ed98da2fcab7eb
josefchmel <chmel@jchsoft.cz>
2019-11-14 14:20:01 +0100

    fixed initializer for running on puma server

--- lib/generators/midget_jobs/templates/midget_job_initializer.rb
+++ lib/generators/midget_jobs/templates/midget_job_initializer.rb
@@ -1,7 +1,7 @@
 Rails.application.config.active_job.queue_adapter = :midget_job
 Rails.application.config.x.midget_jobs.at_once = 4
 
-if Rails.const_defined? 'Server'
+if Rails.const_defined?('Server') || $0.include?('puma')
   Rails.application.config.after_initialize do
     MidgetJobs.initialize_workers
   end
