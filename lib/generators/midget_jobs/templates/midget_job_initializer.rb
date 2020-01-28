7713aa405d2f5c60ad6d46ffeb3c4a0c67d810ed
josefchmel <chmel@jchsoft.cz>
2020-01-28 15:53:14 +0100

    fix deprecation warning about class.parent_name -> class.module_parent_name

--- lib/generators/midget_jobs/templates/midget_job_initializer.rb
+++ lib/generators/midget_jobs/templates/midget_job_initializer.rb
@@ -1,6 +1,6 @@
 Rails.application.config.active_job.queue_adapter = :midget_job
 Rails.application.config.x.midget_jobs.at_once = 4
-Rails.application.config.x.midget_jobs.runner = Rails.application.class.parent_name
+Rails.application.config.x.midget_jobs.runner = Rails.application.class.module_parent_name
 
 if Rails.const_defined?('Server') || $0.include?('puma')
   Rails.application.config.after_initialize do
