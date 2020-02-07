3559e04340943b89789f44b8ed22a8b6eac5223f
josefchmel <chmel@jchsoft.cz>
2020-02-07 15:53:58 +0100

    Rubocop optimizing changes - removed unnecessary

--- lib/models/concerns/postgres_notifications_listener.rb
+++ lib/models/concerns/postgres_notifications_listener.rb
@@ -2,22 +2,20 @@
 
 module PostgresNotificationsListener
   def listen_notifications
-    Rails.logger.info "#{self.name} listening_job started"
+    Rails.logger.info "#{name} listening_job started"
     Thread.new do
-      Rails.logger.info "#{self.name} listening_job running on #{self.name.pluralize.underscore}_notices"
-      self.connection.execute "LISTEN #{self.name.pluralize.underscore}_notices"
+      Rails.logger.info "#{name} listening_job running on #{name.pluralize.underscore}_notices"
+      connection.execute "LISTEN #{name.pluralize.underscore}_notices"
       loop do
-        begin
-          Rails.logger.info "#{self.name} listening_job wait_for_notify"
-          self.connection.raw_connection.wait_for_notify do |event, pid, payload|
-            data = JSON.parse payload, symbolize_names: true
-            Rails.logger.info "postgres #{event.inspect}, pid: #{pid.inspect}, data: #{data.inspect}"
-            process_notification(data)
-          end
-        rescue StandardError => error
-          Rails.logger.error "-------------Ex #{self.name} listening_job wait_for_notify #{error}----------------"
-          retry
+        Rails.logger.info "#{name} listening_job wait_for_notify"
+        connection.raw_connection.wait_for_notify do |event, pid, payload|
+          data = JSON.parse payload, symbolize_names: true
+          Rails.logger.info "postgres #{event.inspect}, pid: #{pid.inspect}, data: #{data.inspect}"
+          process_notification(data)
         end
+      rescue StandardError => e
+        Rails.logger.error "-----------Ex #{name} listening_job wait_for_notify #{e}-retrying!------------"
+        retry
       end
     end.abort_on_exception = true
   end
