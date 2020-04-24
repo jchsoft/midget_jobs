37198ff562962f3ad39ac521366ee84065650419
josefchmel <chmel@jchsoft.cz>
2020-04-24 12:23:06 +0200

    handle PG::ConnectionBad socket error by reestablishing AR connection

--- lib/models/concerns/postgres_notifications_listener.rb
+++ lib/models/concerns/postgres_notifications_listener.rb
@@ -13,8 +13,16 @@ module PostgresNotificationsListener
           Rails.logger.info "postgres #{event.inspect}, pid: #{pid.inspect}, data: #{data.inspect}"
           process_notification(data)
         end
+      rescue ActiveRecord::StatementInvalid => e
+        Rails.logger.error "Ex #{name} listening_job wait_for_notify #{e}-retrying!"
+        if e.to_s.include?('PG::ConnectionBad')
+          Rails.logger.error "Reestablish AR connection..."
+          ActiveRecord::Base.connection.verify!
+        end
+        sleep 0.1
+        retry
       rescue StandardError => e
-        Rails.logger.error "-----------Ex #{name} listening_job wait_for_notify #{e}-retrying!------------"
+        Rails.logger.error "Ex #{name} listening_job wait_for_notify #{e}-retrying!"
         retry
       end
     end.abort_on_exception = true
