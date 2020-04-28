520a484e18dc738071c63dc41efe3e8e78bd4605
josefchmel <chmel@jchsoft.cz>
2020-04-28 08:46:33 +0200

    figuring out PG::ConnectionBad socket error - better log output

--- lib/models/concerns/postgres_notifications_listener.rb
+++ lib/models/concerns/postgres_notifications_listener.rb
@@ -6,6 +6,7 @@ module PostgresNotificationsListener
     Thread.new do
       Rails.logger.info "#{name} listening_job running on #{name.pluralize.underscore}_notices"
       connection.execute "LISTEN #{name.pluralize.underscore}_notices"
+      exception_counter ||= 0
       loop do
         Rails.logger.info "#{name} listening_job wait_for_notify"
         connection.raw_connection.wait_for_notify do |event, pid, payload|
@@ -13,17 +14,21 @@ module PostgresNotificationsListener
           Rails.logger.info "postgres #{event.inspect}, pid: #{pid.inspect}, data: #{data.inspect}"
           process_notification(data)
         end
+        exception_counter = 0
       rescue ActiveRecord::StatementInvalid => e
-        Rails.logger.error "Ex #{name} listening_job wait_for_notify #{e}-retrying!"
-        if e.to_s.include?('PG::ConnectionBad')
+        exception_counter += 1
+        Rails.logger.error "Ex #{name} listening_job wait_for_notify #{e.inspect}-retrying!"
+        if e.to_s.include?('PQsocket')
           Rails.logger.error "Reestablish AR connection..."
           ActiveRecord::Base.connection.verify!
         end
         sleep 0.1
-        retry
+        (exception_counter < 10) ? retry : raise(e)
       rescue StandardError => e
-        Rails.logger.error "Ex #{name} listening_job wait_for_notify #{e}-retrying!"
-        retry
+        exception_counter += 1
+        Rails.logger.error "Ex #{name} listening_job wait_for_notify #{e.inspect}-retrying!"
+        sleep 0.1
+        (exception_counter < 10) ? retry : raise(e)
       end
     end.abort_on_exception = true
   end
