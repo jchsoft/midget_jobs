7f01f0018205a7eca876e53521cf9b67c6c030b0
josefchmel <chmel@jchsoft.cz>
2020-05-27 17:01:29 +0200

    trying to fix PG::ConnectionBad error on listening_job

--- lib/midget_jobs/version.rb
+++ lib/midget_jobs/version.rb
@@ -1,3 +1,3 @@
 module MidgetJobs
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 end
--- lib/models/concerns/postgres_notifications_listener.rb
+++ lib/models/concerns/postgres_notifications_listener.rb
@@ -15,20 +15,18 @@ module PostgresNotificationsListener
           process_notification(data)
         end
         exception_counter = 0
-      rescue ActiveRecord::StatementInvalid => e
+      rescue PG::ConnectionBad => e
         exception_counter += 1
-        Rails.logger.error "Ex #{name} listening_job wait_for_notify #{e.inspect}-retrying!"
-        if e.to_s.include?('PQsocket')
-          Rails.logger.error "Reestablish AR connection..."
-          ActiveRecord::Base.connection.verify!
-        end
+        Rails.logger.error "#{name} listening_job wait_for_notify lost connection #{e.inspect} retry #{exception_counter}th time!"
+        Rails.logger.error "Reestablish AR connection..."
+        ActiveRecord::Base.connection.verify!
         sleep 0.1
-        (exception_counter < 10) ? retry : raise(e)
+        (exception_counter < 3) ? retry : raise(e)
       rescue StandardError => e
         exception_counter += 1
-        Rails.logger.error "Ex #{name} listening_job wait_for_notify #{e.inspect}-retrying!"
+        Rails.logger.error "#{name} listening_job wait_for_notify exception #{e.inspect} retry #{exception_counter}th time!"
         sleep 0.1
-        (exception_counter < 10) ? retry : raise(e)
+        (exception_counter < 3) ? retry : raise(e)
       end
     end.abort_on_exception = true
   end
